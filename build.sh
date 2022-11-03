6c160b4b33a0d96de1a5102b6c5b387cafbc8bd7
Caio Barbosa <caio.barbosa@virtus.ufcg.edu.br>
2022-11-03 11:43:20 -0300

    fix

--- build.sh
+++ build.sh
@@ -10,5 +10,3 @@ python -m pip install --force-reinstall -U setuptools
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
-python manage.py starter --username caio --password oficinag3 --noinput --email caiomarinho8@gmail.com
-python manage.py starter --username admin --password Admin123! --noinput --email admin@gmail.com
